5a9a1643646b8e9c56214a83b2c8497096a89b24
Eliot Glairon <ejg_dnd@yahoo.com>
2016-10-02 20:13:42 +0000

    Added mongo and a limited feature test

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var app = express();
-
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+var mongo = require('mongodb').MongoClient
+var maxnumber = 0;
+app.get('/new/*', function (req, res) {
+  mongo.connect("mongodb://localhost:27017/learnyoumongo", function(err, db) {
+    if (err) {
+      console.error(err.toString());
+      return;
+    }
+    res.set({'Content-Type': 'application/json'});
+    res.send({
+      "shortenedurl": req.headers.host + "/1",
+      "originalurl":req.headers.host + req.path
+    });
+  });
 });
+
+app.get('/v/:UNIQUEID', function (req, res) {
+  res.send(req.params.UNIQUEID);
+});
+
 
 app.listen(process.env.PORT, function () {
   console.log('Example app listening on port ' + process.env.PORT + '!');
