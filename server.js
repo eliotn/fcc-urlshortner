9b99fabccb0334c78f2e1ee532e152867973d6fb
Eliot Glairon <ejg_dnd@yahoo.com>
2016-10-03 15:30:18 +0000

    Url shortner works but which parameters should I use?

--- server.js
+++ server.js
@@ -1,23 +1,85 @@
 var express = require('express');
 var app = express();
-var mongo = require('mongodb').MongoClient
-var maxnumber = 0;
+var mongo = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectId; 
+var url = require('url')
+var largeprime = 492876289;
 app.get('/new/*', function (req, res) {
+  res.set({'Content-Type': 'application/json'});
+  var extractedurl = req.path.slice(5);
+  //TODO: Maybe this coud be better
+  if (!url.parse(extractedurl).protocol) {
+    res.send({"error":"The url does not contain a valid protocol!"});
+    return;
+  }
+  mongo.connect("mongodb://localhost:27017/learnyoumongo", function(err, db) {
+    if (err) {
+      console.error(err.toString());
+      res.send("I could not generate a new url!");
+      db.close();
+      return;
+    }
+    //add a blank entry first, then fetch the id and use
+    //that to show where to access everything
+    
+    var urlobj = {
+        "originalurl":extractedurl
+    };
+    db.collection('urls').insert(urlobj, function(err) {
+      if (err) {
+        res.send({"error":err.toString()});
+        return;
+      }
+      else {
+        console.log(urlobj._id);
+        res.send({
+          "shortenedurl": req.headers.host + "/v/" + urlobj._id,
+          "originalurl": urlobj.originalurl
+        });
+      }
+      db.close();
+    })
+    
+    
+  });
+});
+
+app.get('/v/:UNIQUEID', function (req, res) {
+  console.log(req.params.UNIQUEID);
   mongo.connect("mongodb://localhost:27017/learnyoumongo", function(err, db) {
     if (err) {
       console.error(err.toString());
       return;
     }
-    res.set({'Content-Type': 'application/json'});
-    res.send({
-      "shortenedurl": req.headers.host + "/1",
-      "originalurl":req.headers.host + req.path
-    });
-  });
-});
-
-app.get('/v/:UNIQUEID', function (req, res) {
-  res.send(req.params.UNIQUEID);
+    try {
+      var query = {
+        "_id" : {
+          "$in" : [ ObjectId(req.params.UNIQUEID) ]
+        }
+      };
+    }
+    catch (err) {
+      res.send("Could not access the entered url, bad format")
+      db.close()
+    }
+    db.collection("urls").find(query).toArray(function(err, results) {
+      if (err || !results.length) {
+        if (err) {
+          console.error(err.toString());
+        }
+        else {
+          console.log(results.length);
+        }
+        var errstring = "Could not access the entered in url";
+        res.send(errstring);
+        db.close();
+      }
+      else {
+        res.redirect(301, results[0].originalurl);
+        db.close();
+      }
+    }); 
+  });  
 });
 
 
