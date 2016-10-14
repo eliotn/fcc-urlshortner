623dec34bbdeedbec167f8d27a38cce42fdd7951
Eliot Glairon <ejg_dnd@yahoo.com>
2016-10-14 04:44:11 +0000

    URL SHORTNER WORKS WITH INTEGERS!

--- server.js
+++ server.js
@@ -9,15 +9,17 @@ mongo.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/learnyoumong
     console.error(err.toString());
     return;
   }
-  db.collection('urls').createIndex( { "_id": "hashed"}, null,
+  db.collection('counters').insert( { "_id": "urlcounter", "seq":"0"}, null,
   function(err, results) {
-    if (err) {console.error(err.toString());}
-    else {console.log(results);}
+    if (err) {db.close(); return;}//already here
     db.close();
     return;
   });
   //db.collection('urls').createIndex({_id: "hashed"});
 });
+
+
+
 app.use(express.static(__dirname + '/public'));
 app.get('/new/*', function (req, res) {
   res.set({'Content-Type': 'application/json'});
@@ -36,61 +38,68 @@ app.get('/new/*', function (req, res) {
     //add a blank entry first, then fetch the id and use
     //that to show where to access everything
     
-    var urlobj = {
-      "createdAt": new Date(),
-      "originalurl":extractedurl
-    };
+    //hash 
+    var id = -1;
+    db.collection('counters').update(
+    { _id: "urlcounter" },
+    { $inc: { seq: 1 } }, function(err, result) {
+      if (err) { res.send({"error":err.toString()}); }
+      db.collection('counters').find({"_id":"urlcounter"}).toArray(function(err, result) {
+        if (err) { res.send({"error":err.toString()}); }
+        id = result[0].seq;
+        console.log(id);
+        if (id === -1) {res.send({"error":"could not generate the url: -1"});return;}
+        var urlobj = {
+          "createdAt": new Date(),
+          "originalurl":extractedurl,
+          "_id": id.toString()
+        };
+        db.collection('urls').insert(urlobj, function(err) {
+          if (err) {
+            res.send({"error":err.toString()});
+            return;
+          }
+          else {
+            console.log(urlobj);
+            console.log(urlobj._id);
+            db.close();
+            res.send({
+              "shortenedurl": req.headers.host + "/v/" + urlobj._id,
+              "originalurl": urlobj.originalurl
+            });
+          }
+          
+        })
+      });
+    });
     
-    db.collection('urls').insert(urlobj, function(err) {
-      if (err) {
-        res.send({"error":err.toString()});
-        return;
-      }
-      else {
-        console.log(urlobj._id);
-        res.send({
-          "shortenedurl": req.headers.host + "/v/" + urlobj._id_hashed,
-          "originalurl": urlobj.originalurl
-        });
-      }
-      db.close();
-    })
     
     
   });
 });
 
 app.get('/v/:UNIQUEID', function (req, res) {
-  console.log(req.params.UNIQUEID);
   mongo.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/learnyoumongo", {native_parser:true}, function(err, db) {
     if (err) {
       console.error(err.toString());
       return;
     }
-    try {
-      var query = {
-        "_id" : {
-          "$in" : [ ObjectId(req.params.UNIQUEID) ]
-        }
-      };
-    }
-    catch (err) {
-      res.send("Could not access the entered url, bad format")
-      db.close()
-    }
-    db.collection("urls").find(query).toArray(function(err, results) {
+    //"_id":req.params.UNIQUEID
+    db.collection("urls").find({"_id":req.params.UNIQUEID.toString()}).toArray(function(err, results) {
       if (err || !results.length) {
         if (err) {
-          console.error(err.toString());
+          console.error("Error: " + err.toString());
         }
         else {
-          console.log(results.length);
+          console.log("Length" + results.length);
         }
         var errstring = "Could not access the entered in url";
         res.send(errstring);
         db.close();
       }
       else {
+        console.log(results)
+        console.log(results[0].originalurl);
         res.redirect(301, results[0].originalurl);
         db.close();
       }
